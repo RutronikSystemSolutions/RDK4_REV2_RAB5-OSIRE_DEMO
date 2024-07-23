1c2540692cf8a42f4f324f08ba58d25113beaa0e
Rutronik Elektronische Bauelemente GmbH <46448552+RutronikKaunas@users.noreply.github.com>
2024-07-23 13:24:43 +0300

    project cleanup.

--- bsps/TARGET_APP_RDK4/docs/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['cy8ckit_2d041s_2dmax_20bsp_0',['CY8CKIT-041S-MAX BSP',['../index.html',1,'']]]
-];
--- bsps/TARGET_APP_RDK4/docs/html/search/pages_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['peripheral_20default_20bsp_20settings_0',['Peripheral Default BSP Settings',['../md_source_bsps_cat2_CY8CKIT_041S_MAX_bsp_settings.html',1,'']]]
-];
